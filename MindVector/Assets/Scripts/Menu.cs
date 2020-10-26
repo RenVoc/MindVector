40317fbe7e08a1b146e0f54237cb93f2d7bace3c
Ivan Olovyannikov <iolovyannikov@yandex.ru>
2020-10-26 14:27:07 +0300

    initial

---
+++ MindVector/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startFunction()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void History()
+    {
+        SceneManager.LoadScene("History");
+    }
+    public void Achievement()
+    {
+        SceneManager.LoadScene("Achievement"); 
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void Authors()
+    {
+        SceneManager.LoadScene("Authors");
+    }
+}
